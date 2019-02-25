ff80d04df60f7604efc26315b744f6a990dff53a
svndevelopment <47905966+svndevelopment@users.noreply.github.com>
2019-02-25 18:31:25 +0100

    4.1.4.0

---
+++ Extensions.cs
@@ -0,0 +1,14 @@
+﻿using System.Drawing;
+
+namespace SVN.Drawing
+{
+    public static class Extensions
+    {
+        public static double Sigmoid(this Color param)
+        {
+            var value = param.R + param.G + param.B;
+            var valueMax = byte.MaxValue * 3;
+            return (double)value / valueMax;
+        }
+    }
+}
