3de2fa9fb657785df73614d3059c559ba2fd0cdc
svndevelopment <47905966+svndevelopment@users.noreply.github.com>
2019-03-02 19:46:47 +0100

    4.1.9.0

--- Extensions.cs
+++ Extensions.cs
@@ -1,14 +1,327 @@
-﻿using System.Drawing;
+﻿using SVN.Math2;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
 
 namespace SVN.Drawing
 {
     public static class Extensions
     {
+        public static string GetResolution(this byte[] param)
+        {
+            using (var image = param.ToImage())
+            {
+                return image.GetResolution();
+            }
+        }
+
+        public static string GetResolution(this Image param)
+        {
+            return $"{param.Width} X {param.Height}";
+        }
+
+        public static void ApplyRatio(this Image param, Size size)
+        {
+            var ratio = (double)param.Width / param.Height;
+
+            if (ratio < 0)
+            {
+                size.Height = (int)(size.Width * ratio);
+            }
+            else
+            {
+                size.Width = (int)(size.Height * ratio);
+            }
+        }
+
+        public static byte[] Resize(this byte[] param, int width, int height)
+        {
+            return param.Resize(new Size(width, height));
+        }
+
+        public static byte[] Resize(this byte[] param, Size size)
+        {
+            if (param.Length == 0)
+            {
+                return param;
+            }
+            using (var image = param.ToImage())
+            {
+                if (size.Width > image.Width || size.Height > image.Height) return param;
+
+                image.ApplyRatio(size);
+                using (var destImage = new Bitmap(size.Width, size.Height))
+                {
+                    destImage.SetResolution(image.HorizontalResolution, image.VerticalResolution);
+                    image.DrawGraphics(destImage);
+                    return destImage.ToBytes();
+                }
+            }
+        }
+
+        public static void DrawGraphics(this Image param, Image destImage)
+        {
+            using (var graphics = Graphics.FromImage(destImage))
+            {
+                graphics.CompositingMode = CompositingMode.SourceCopy;
+                graphics.CompositingQuality = CompositingQuality.HighQuality;
+                graphics.InterpolationMode = InterpolationMode.HighQualityBicubic;
+                graphics.SmoothingMode = SmoothingMode.HighQuality;
+                graphics.PixelOffsetMode = PixelOffsetMode.HighQuality;
+
+                using (var attributes = new ImageAttributes())
+                {
+                    attributes.SetWrapMode(WrapMode.TileFlipXY);
+                    var srcRect = new Rectangle(Point.Empty, param.Size);
+                    var destRect = new Rectangle(Point.Empty, destImage.Size);
+                    graphics.DrawImage(param, destRect, srcRect, GraphicsUnit.Pixel, attributes);
+                }
+            }
+        }
+
+        public static void DrawImage(this Graphics param, Image image, Rectangle destRect, Rectangle srcRect, GraphicsUnit srcUnit, ImageAttributes imageAttr)
+        {
+            param.DrawImage(image, destRect, srcRect.X, srcRect.Y, srcRect.Width, srcRect.Height, srcUnit, imageAttr);
+        }
+
+        public static Bitmap[] Split(this Bitmap param)
+        {
+            var result = new List<Bitmap>();
+
+            var width = ((double)param.Width / 3).FloorToInt();
+            var height = ((double)param.Height / 3).FloorToInt();
+
+            for (var j = 1; j <= 3; j++)
+            {
+                for (var i = 1; i <= 3; i++)
+                {
+                    var bitmap = new Bitmap(width, height);
+
+                    var offsetX = (i - 1) * width;
+                    var offsetY = (j - 1) * height;
+
+                    for (var y = 1; y <= height; y++)
+                    {
+                        for (var x = 1; x <= width; x++)
+                        {
+                            var color = param.GetPixel(offsetX + x - 1, offsetY + y - 1);
+                            bitmap.SetPixel(x - 1, y - 1, color);
+                        }
+                    }
+
+                    result.Add(bitmap);
+                }
+            }
+
+            return result.ToArray();
+        }
+
         public static double Brightness(this Color param)
         {
             var value = param.R + param.G + param.B;
             var valueMax = byte.MaxValue * 3;
             return (double)value / valueMax;
+        }
+
+        public static double Brightness(this Bitmap param)
+        {
+            var result = new List<double>();
+
+            for (var y = 1; y <= param.Height; y++)
+            {
+                for (var x = 1; x <= param.Width; x++)
+                {
+                    var color = param.GetPixel(x - 1, y - 1);
+                    var brightness = color.Brightness();
+                    result.Add(brightness);
+                }
+            }
+
+            return result.Average();
+        }
+
+        public static double[] Brightnesses(this Bitmap param)
+        {
+            var result = new List<double>();
+
+            foreach (var bitmap in param.Split())
+            {
+                var brightness = bitmap.Brightness();
+                result.Add(brightness);
+            }
+
+            return result.Normalize().ToArray();
+        }
+
+        public static double Noise(this Bitmap param)
+        {
+            using (var bitmap = param.ConvolutionFilter())
+            {
+                return bitmap.Brightness();
+            }
+        }
+
+        public static double[] Noises(this Bitmap param)
+        {
+            var result = new List<double>();
+
+            foreach (var bitmap in param.Split())
+            {
+                var noise = bitmap.Noise();
+                result.Add(noise);
+            }
+
+            return result.Normalize().ToArray();
+        }
+
+        public static double[] NoisesHorizontal(this Bitmap param)
+        {
+            var result = new List<double>();
+            var noises = param.Noises();
+
+            for (var i = 1; i <= 3; i++)
+            {
+                var noise1 = noises[(i - 1) * 3 + 0];
+                var noise2 = noises[(i - 1) * 3 + 1];
+                var noise3 = noises[(i - 1) * 3 + 2];
+                var noise = (noise1 + noise2 + noise3) / 3;
+                result.Add(noise);
+            }
+
+            return result.Normalize().ToArray();
+        }
+
+        public static double[] NoisesVertical(this Bitmap param)
+        {
+            var result = new List<double>();
+            var noises = param.Noises();
+
+            for (var i = 1; i <= 3; i++)
+            {
+                var noise1 = noises[i - 1 + 0];
+                var noise2 = noises[i - 1 + 3];
+                var noise3 = noises[i - 1 + 6];
+                var noise = (noise1 + noise2 + noise3) / 3;
+                result.Add(noise);
+            }
+
+            return result.Normalize().ToArray();
+        }
+
+        public static byte[] ToBytes(this Image param)
+        {
+            using (var ms = new MemoryStream())
+            {
+                param.Save(ms, ImageFormat.Jpeg);
+                return ms.ToArray();
+            }
+        }
+
+        public static Image ToImage(this byte[] param)
+        {
+            if (param.Length == 0)
+            {
+                return new Bitmap(1, 1);
+            }
+            return Image.FromStream(new MemoryStream(param));
+        }
+
+        public static string ToBase64String(this byte[] param)
+        {
+            return "data:image;base64," + Convert.ToBase64String(param);
+        }
+
+        public static Bitmap ConvolutionFilter(this Bitmap param, double factor = 1, int bias = 0, bool grayscale = true)
+        {
+            var filterMatrix = new double[,]
+            {
+                { -1, -1, -1, },
+                { -1,  8, -1, },
+                { -1, -1, -1, },
+            };
+            
+            var sourceRect = new Rectangle(default(int), default(int), param.Width, param.Height);
+            var sourceData = param.LockBits(sourceRect, ImageLockMode.ReadOnly, PixelFormat.Format32bppArgb);
+            var pixelBuffer = new byte[sourceData.Stride * sourceData.Height];
+            var resultBuffer = new byte[sourceData.Stride * sourceData.Height];
+
+            Marshal.Copy(sourceData.Scan0, pixelBuffer, default(int), pixelBuffer.Length);
+            param.UnlockBits(sourceData);
+
+            if (grayscale)
+            {
+                for (var i = 1; i <= pixelBuffer.Length; i += 4)
+                {
+                    var rgb = default(double);
+
+                    rgb += pixelBuffer[i - 1] * 0.11f;
+                    rgb += pixelBuffer[i + 0] * 0.59f;
+                    rgb += pixelBuffer[i + 1] * 0.30f;
+
+                    pixelBuffer[i - 1] = (byte)rgb;
+                    pixelBuffer[i + 0] = pixelBuffer[i - 1];
+                    pixelBuffer[i + 1] = pixelBuffer[i - 1];
+                    pixelBuffer[i + 2] = byte.MaxValue;
+                }
+            }
+
+            var filterWidth = filterMatrix.GetLength(1);
+            var filterOffset = (filterWidth - 1) / 2;
+
+            for (var offsetY = filterOffset; offsetY < param.Height - filterOffset; offsetY++)
+            {
+                for (var offsetX = filterOffset; offsetX < param.Width - filterOffset; offsetX++)
+                {
+                    var r = default(double);
+                    var g = default(double);
+                    var b = default(double);
+
+                    var byteOffset = offsetY * sourceData.Stride + offsetX * 4;
+
+                    for (var y = -filterOffset; y <= filterOffset; y++)
+                    {
+                        for (var x = -filterOffset; x <= filterOffset; x++)
+                        {
+                            var offset = byteOffset + (x * 4) + (y * sourceData.Stride);
+                            b += pixelBuffer[offset + 0] * filterMatrix[y + filterOffset, x + filterOffset];
+                            g += pixelBuffer[offset + 1] * filterMatrix[y + filterOffset, x + filterOffset];
+                            r += pixelBuffer[offset + 2] * filterMatrix[y + filterOffset, x + filterOffset];
+                        }
+                    }
+
+                    r = factor * r + bias;
+                    g = factor * g + bias;
+                    b = factor * b + bias;
+
+                    r = Math.Max(r, byte.MinValue);
+                    r = Math.Min(r, byte.MaxValue);
+                    g = Math.Max(g, byte.MinValue);
+                    g = Math.Min(g, byte.MaxValue);
+                    b = Math.Max(b, byte.MinValue);
+                    b = Math.Min(b, byte.MaxValue);
+
+                    resultBuffer[byteOffset + 0] = (byte)b;
+                    resultBuffer[byteOffset + 1] = (byte)g;
+                    resultBuffer[byteOffset + 2] = (byte)r;
+                    resultBuffer[byteOffset + 3] = byte.MaxValue;
+                }
+            }
+
+            using (var resultBitmap = new Bitmap(param.Width, param.Height))
+            {
+                var resultRect = new Rectangle(default(int), default(int), resultBitmap.Width, resultBitmap.Height);
+                var resultData = resultBitmap.LockBits(resultRect, ImageLockMode.WriteOnly, PixelFormat.Format32bppArgb);
+
+                Marshal.Copy(resultBuffer, default(int), resultData.Scan0, resultBuffer.Length);
+                resultBitmap.UnlockBits(resultData);
+
+                return new Bitmap(resultBitmap);
+            }
         }
     }
 }
--- Helpers/ImageData.cs
+++ Helpers/ImageData.cs
@@ -1,56 +1,74 @@
-﻿using SVN.Math2;
-using System;
-using System.Collections.Generic;
+﻿using System;
 using System.Drawing;
-using System.Drawing.Imaging;
 using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices;
 
 namespace SVN.Drawing.Helpers
 {
-    public class ImageData
+    public class ImageData : IDisposable
     {
-        public int Width { get; private set; }
-        public int Height { get; private set; }
-        private List<Color> Colors { get; } = new List<Color>();
+        private Bitmap Bitmap { get; set; }
 
-        private Bitmap Bitmap
+        public int Width
         {
-            get
-            {
-                var result = new Bitmap(this.Width, this.Height);
+            get => this.Bitmap.Width;
+        }
 
-                var i = default(int);
-                foreach (var color in this.Colors)
-                {
-                    var x = i % this.Width;
-                    var y = ((double)i / this.Width).FloorToInt();
-                    result.SetPixel(x, y, color);
-                    i++;
-                }
+        public int Height
+        {
+            get => this.Bitmap.Height;
+        }
 
-                return result;
-            }
+        public double Brightness
+        {
+            get => this.Bitmap.Brightness();
+        }
+
+        public double[] Brightnesses
+        {
+            get => this.Bitmap.Brightnesses();
+        }
+
+        public double Noise
+        {
+            get => this.Bitmap.Noise();
+        }
+
+        public double[] Noises
+        {
+            get => this.Bitmap.Noises();
+        }
+
+        public double[] NoisesHorizontal
+        {
+            get => this.Bitmap.NoisesHorizontal();
+        }
+
+        public double[] NoisesVertical
+        {
+            get => this.Bitmap.NoisesVertical();
         }
 
         private ImageData()
         {
         }
 
-        public static ImageData From(Bitmap bitmap)
+        public void Dispose()
         {
-            var input = new ImageData();
-            input.Apply(bitmap);
-            return input;
+            this.Bitmap.Dispose();
+            this.Bitmap = null;
+        }
+
+        public static ImageData FromBitmap(Bitmap bitmap)
+        {
+            return new ImageData
+            {
+                Bitmap = bitmap,
+            };
         }
 
         public static ImageData FromFile(string path)
         {
-            using (var bitmap = new Bitmap(path))
-            {
-                return ImageData.From(bitmap);
-            }
+            return ImageData.FromBitmap(new Bitmap(path));
         }
 
         public void ToFile(string path)
@@ -62,163 +80,19 @@ namespace SVN.Drawing.Helpers
                 Directory.CreateDirectory(directory);
             }
 
-            using (var bitmap = this.Bitmap)
-            {
-                bitmap.Save(path);
-            }
-        }
-
-        private void SetPixels(Bitmap bitmap)
-        {
-            this.Colors.Clear();
-
-            for (var y = 1; y <= bitmap.Height; y++)
-            {
-                for (var x = 1; x <= bitmap.Width; x++)
-                {
-                    this.Colors.Add(bitmap.GetPixel(x - 1, y - 1));
-                }
-            }
-        }
-
-        private void Apply(Bitmap bitmap)
-        {
-            this.Width = bitmap.Width;
-            this.Height = bitmap.Height;
-            this.SetPixels(bitmap);
+            this.Bitmap.Save(path);
         }
 
         public void ApplyConvolutionFilter(double factor = 1, int bias = 0, bool grayscale = true)
         {
-            var filterMatrix = new double[,]
+            var bitmap1 = this.Bitmap;
+            
+            using (var bitmap2 = new Bitmap(bitmap1))
             {
-                { -1, -1, -1, },
-                { -1,  8, -1, },
-                { -1, -1, -1, },
-            };
-
-            var bitmap = this.Bitmap;
-            var sourceRect = new Rectangle(default(int), default(int), bitmap.Width, bitmap.Height);
-            var sourceData = bitmap.LockBits(sourceRect, ImageLockMode.ReadOnly, PixelFormat.Format32bppArgb);
-            var pixelBuffer = new byte[sourceData.Stride * sourceData.Height];
-            var resultBuffer = new byte[sourceData.Stride * sourceData.Height];
-
-            Marshal.Copy(sourceData.Scan0, pixelBuffer, default(int), pixelBuffer.Length);
-            bitmap.UnlockBits(sourceData);
-
-            if (grayscale)
-            {
-                for (var i = 1; i <= pixelBuffer.Length; i += 4)
-                {
-                    var rgb = default(double);
-
-                    rgb += pixelBuffer[i - 1] * 0.11f;
-                    rgb += pixelBuffer[i + 0] * 0.59f;
-                    rgb += pixelBuffer[i + 1] * 0.30f;
-
-                    pixelBuffer[i - 1] = (byte)rgb;
-                    pixelBuffer[i + 0] = pixelBuffer[i - 1];
-                    pixelBuffer[i + 1] = pixelBuffer[i - 1];
-                    pixelBuffer[i + 2] = byte.MaxValue;
-                }
+                this.Bitmap = bitmap2.ConvolutionFilter(factor, bias, grayscale);
             }
 
-            var filterWidth = filterMatrix.GetLength(1);
-            var filterOffset = (filterWidth - 1) / 2;
-
-            for (var offsetY = filterOffset; offsetY < bitmap.Height - filterOffset; offsetY++)
-            {
-                for (var offsetX = filterOffset; offsetX < bitmap.Width - filterOffset; offsetX++)
-                {
-                    var r = default(double);
-                    var g = default(double);
-                    var b = default(double);
-
-                    var byteOffset = offsetY * sourceData.Stride + offsetX * 4;
-
-                    for (var y = -filterOffset; y <= filterOffset; y++)
-                    {
-                        for (var x = -filterOffset; x <= filterOffset; x++)
-                        {
-                            var offset = byteOffset + (x * 4) + (y * sourceData.Stride);
-                            b += pixelBuffer[offset + 0] * filterMatrix[y + filterOffset, x + filterOffset];
-                            g += pixelBuffer[offset + 1] * filterMatrix[y + filterOffset, x + filterOffset];
-                            r += pixelBuffer[offset + 2] * filterMatrix[y + filterOffset, x + filterOffset];
-                        }
-                    }
-
-                    r = factor * r + bias;
-                    g = factor * g + bias;
-                    b = factor * b + bias;
-
-                    r = Math.Max(r, byte.MinValue);
-                    r = Math.Min(r, byte.MaxValue);
-                    g = Math.Max(g, byte.MinValue);
-                    g = Math.Min(g, byte.MaxValue);
-                    b = Math.Max(b, byte.MinValue);
-                    b = Math.Min(b, byte.MaxValue);
-
-                    resultBuffer[byteOffset + 0] = (byte)b;
-                    resultBuffer[byteOffset + 1] = (byte)g;
-                    resultBuffer[byteOffset + 2] = (byte)r;
-                    resultBuffer[byteOffset + 3] = byte.MaxValue;
-                }
-            }
-
-            using (var resultBitmap = new Bitmap(bitmap.Width, bitmap.Height))
-            {
-                var resultRect = new Rectangle(default(int), default(int), resultBitmap.Width, resultBitmap.Height);
-                var resultData = resultBitmap.LockBits(resultRect, ImageLockMode.WriteOnly, PixelFormat.Format32bppArgb);
-
-                Marshal.Copy(resultBuffer, default(int), resultData.Scan0, resultBuffer.Length);
-                resultBitmap.UnlockBits(resultData);
-
-                this.Apply(resultBitmap);
-            }
-        }
-
-        private double GetNoise(Bitmap bitmap, int widthIndex, int heightIndex, int width, int height)
-        {
-            var result = new List<double>();
-
-            var offsetX = widthIndex * width;
-            var offsetY = heightIndex * height;
-
-            for (var x = 1; x <= width; x++)
-            {
-                for (var y = 1; y <= height; y++)
-                {
-                    var color = bitmap.GetPixel(offsetX + x - 1, offsetY + y - 1);
-                    var brightness = color.Brightness();
-                    result.Add(brightness);
-                }
-            }
-
-            return result.Average();
-        }
-
-        public double[] GetNoises()
-        {
-            var result = new List<double>();
-
-            using (var bitmap = this.Bitmap)
-            {
-                var width = ((double)this.Width / 3).FloorToInt();
-                var height = ((double)this.Height / 3).FloorToInt();
-
-                for (var y = 1; y <= 3; y++)
-                {
-                    for (var x = 1; x <= 3; x++)
-                    {
-                        var noise = this.GetNoise(bitmap, x - 1, y - 1, width, height);
-                        result.Add(noise);
-                    }
-                }
-            }
-
-            var maxNoise = result.Max();
-            result = result.Select(x => x / maxNoise).ToList();
-            return result.ToArray();
+            bitmap1.Dispose();
         }
     }
 }
